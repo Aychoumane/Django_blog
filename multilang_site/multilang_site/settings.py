a1bc02b0a36fd62604ac7465d45d7b120d3b5a54
Aychoumane <158152118+Aychoumane@users.noreply.github.com>
2024-06-30 19:13:58 +0200

    Add files via upload

--- multilang_site/multilang_site/settings.py
+++ multilang_site/multilang_site/settings.py
@@ -50,6 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware'
 
 ]
 
@@ -132,6 +133,10 @@ OPENAI_API_KEY = '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'
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# Simplifiez le stockage des fichiers statiques avec Whitenoise
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
